adb7193fa8f25d0a8b773ce3e696ae458b03dd1c
voicehovski <help.kiev@gmail.com>
2022-03-31 12:54:32 +0300

    Initial

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+package main.java;
+
+import java.util.Arrays;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.deepToString(args));
+    }
+}
