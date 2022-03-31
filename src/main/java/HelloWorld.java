0bc590361c2512469568fc6c139b1d981eeea41d
voicehovski <help.kiev@gmail.com>
2022-03-31 13:23:28 +0300

    First test mod

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,5 +5,6 @@ import java.util.Arrays;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("So that`s that");
     }
 }
